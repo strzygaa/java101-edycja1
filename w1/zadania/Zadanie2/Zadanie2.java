477300ad8a2a6cc9377b437183210d3e794e9ce9
strzygaa <paulina.otlik@gmail.com>
2023-02-27 19:09:46 +0100

    Wykonanie zadania 1 i 2 z wykładu nr1

---
+++ w1/zadania/Zadanie2/Zadanie2.java
@@ -0,0 +1,20 @@
+package w1.zadania.Zadanie2;
+import static java.lang.Math.*;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int a =5;
+        int b=10;
+        double c=pow(a,b);
+        double d=sqrt(c);
+        System.out.print(a +" "+b+" "+c+" "+d);
+        int e =-5;
+        double g =abs(e);
+        double h=10.5001;
+        h=round(h);
+        System.out.print(a +" "+b+" "+c+" "+d+" "+e+" "+g+" "+h);
+
+
+
+    }
+}
